7cc3fee6a1f010ca6ce16cf6897cf59db939c45a
albertrajmca <albertrajmca@gmail.com>
2019-02-27 13:32:47 +0530

    first commit
    first commit for this repo

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Devalby\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('Devalby\Calculator\CalculatorController');
+        $this->loadViewsFrom(__DIR__.'/views', 'calculator');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+    }
+}
